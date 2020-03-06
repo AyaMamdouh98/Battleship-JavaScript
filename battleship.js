a03773521e6f679d5d1a00ea080d787a9294ec05
AyaMamdouh98 <51255255+AyaMamdouh98@users.noreply.github.com>
2020-03-06 23:23:02 +0200

    Add files via upload

--- battleship.js
+++ battleship.js
@@ -70,13 +70,13 @@ function fireTorpedo(e) {
 				
 		// if player clicks a square with no ship, change the color and change square's value
 		if (gameBoard[row][col] == 0) {
-			e.target.style.background = '#bbb';
+			e.target.style.background = 'rgb(209, 79, 155)';
 			// set this square's value to 3 to indicate that they fired and missed
 			gameBoard[row][col] = 3;
 			
 		// if player clicks a square with a ship, change the color and change square's value
 		} else if (gameBoard[row][col] == 1) {
-			e.target.style.background = 'red';
+			e.target.style.background = 'green';
 			// set this square's value to 2 to indicate the ship has been hit
 			gameBoard[row][col] = 2;
 			
---
+++ student.md
@@ -0,0 +1,1 @@
+Name : Aya Mamdouh Mahmoud Elkhatieb.
